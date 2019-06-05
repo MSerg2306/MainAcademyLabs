d825a50fef9d7fbce15fa6aa8428511a7dcfdd0e
MSerg2306 <sergey.marinko@gmail.com>
2019-06-05 12:51:09 +0300

    Work43
    Collections, enumerators and iterators

---
+++ Work43/Animal.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Work43
+{
+    public class Animal : IComparable<Animal>
+    {
+        private string _genus { get; set; }
+        private int _weight { get; set; }
+        public string Genus
+        {
+            get { return _genus; }
+        }
+        public int Weight
+        {
+            get { return _weight; }
+        }
+        public Animal() { }
+        public Animal(string genus, int weight)
+        {
+            this._genus = genus;
+            this._weight = weight;
+        }
+        public override string ToString()
+        {
+            return _genus + "-" + _weight;
+        }
+        public int CompareTo(Animal animal)
+        {
+            return String.Compare(this._genus, animal.Genus);
+        }
+        public static void SortWeightAscending(Animal[] animal)
+        {
+            Array.Sort(animal, new SortWeightAscendingHelper());
+        }
+        public static void SortGenusDescending(Animal[] animal)
+        {
+            Array.Sort(animal, new SortGenusDescendingHelper());
+        }
+
+        internal class SortWeightAscendingHelper : IComparer<Animal>
+        {
+            public int Compare(Animal x, Animal y)
+            {
+                if (x.Weight > y.Weight)
+                    return 1;
+                if (x.Weight < y.Weight)
+                    return -1;
+                else return 0;
+            }
+        }
+        internal class SortGenusDescendingHelper : IComparer<Animal>
+        {
+            public int Compare(Animal x, Animal y)
+            {
+                return String.Compare(y.Genus, x.Genus);
+            }
+        }
+    }
+}
---
+++ Work43/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static Work43.Animal;
+
+namespace Work43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Print animals with foreach operator for object of Animals");
+                Animals animals = new Animals();
+                foreach (Animal a in animals)
+                    Console.WriteLine(a);
+                
+                Animal a1 = new Animal("elephant", 5000);
+                Animal a2 = new Animal("lion", 800);
+                Animal a3 = new Animal("buffalo", 1200);
+                Animal a4 = new Animal("antelope", 75);
+                Animal[] animal = new Animal[] { a1, a2, a3, a4 };
+
+                Console.WriteLine("\nPrint results with foreach operator for array of Animal objects (CompareTo)");
+                Array.Sort(animal);
+                foreach (var a in animal)
+                {
+                    Console.WriteLine(a);
+                }
+
+                Console.WriteLine("\nPrint results with foreach operator for array of Animal objects (SortWeightAscendin)");
+                SortWeightAscending(animal);
+                foreach (var a in animal)
+                {
+                    Console.WriteLine(a);
+                }
+
+                Console.WriteLine("\nPrint results with foreach operator for array of Animal objects (SortGenusDescending)");
+                SortGenusDescending(animal);
+                foreach (var a in animal)
+                {
+                    Console.WriteLine(a);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
